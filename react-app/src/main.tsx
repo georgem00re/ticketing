a8af57b2cf3d44600cfac48deef518a2601411a0
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-10-15 16:34:42 +0100

    commit vite react-app

---
+++ react-app/src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+export default function App() {
+	return (
+		<div>Ticketing System</div>
+	)
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<App/>
+)
