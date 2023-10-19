ab96cf5332aa87acb49679a3118257b99af555d3
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-10-19 16:06:52 +0100

    final changes

--- react-app/src/components/Column.tsx
+++ react-app/src/components/Column.tsx
@@ -8,7 +8,7 @@ interface ColumnProps {
 export default function Column({ title }: ColumnProps) {
 	return (
 		<div class="column has-background-white m-2 box">
-			<h1 class="title is-6">{title}</h1>
+			<h1 class="title is-6 mb-2">{title}</h1>
 			<Divider color="#212121"/>
 		</div>
 	)
