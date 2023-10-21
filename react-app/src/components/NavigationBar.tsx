72a1f8070f929cec78860d234cd135c44dba15f7
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-10-21 23:22:20 +0100

    re-style the navigation bar

--- react-app/src/components/NavigationBar.tsx
+++ react-app/src/components/NavigationBar.tsx
@@ -1,18 +1,25 @@
 
-import { ImTicket } from "react-icons/im"
+import { AiOutlineIssuesClose } from "react-icons/ai"
+import { IoMdAddCircle } from "react-icons/io"
 
 export default function NavigationBar({ onCreateClicked }) {
 	return (
-		<nav class="navbar is-background-white p-0 box">
+		<nav class="navbar is-transparent p-1 box has-bottom-border-grey">
 			<div class="navbar-brand">
 				<div class="navbar-item">
-					<ImTicket fontSize="25px"/>
+					<AiOutlineIssuesClose fontSize="1.20rem" color="white"/>
+					<h1 class="subtitle is-5 ml-2 has-text-white">Trackit</h1>
 				</div>
 			</div>
 			<div class="navbar-menu">
 				<div class="navbar-end">
 					<div class="navbar-item">
-						<button class="button is-link is-small" onClick={() => onCreateClicked()}>Create</button>
+						<button class="button is-link has-text-white" onClick={() => onCreateClicked()}>
+						    <span class="icon is-small">
+						      <IoMdAddCircle fontSize="1.20rem" color="white"/>
+						    </span>
+						    <span>Create</span>
+						</button>
 					</div>
 				</div>
 				
