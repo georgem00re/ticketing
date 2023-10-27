6ff6b102bbbc4d63c9d0a7c61c43f0f4dfe2cafa
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-10-27 19:23:02 +0100

    final changes

--- react-app/src/components/Column.tsx
+++ react-app/src/components/Column.tsx
@@ -8,21 +8,12 @@ interface ColumnProps {
 
 export default function Column({ title, children }: ColumnProps) {
 	return (
-		<div class="column has-background-transparent m-5 p-3" style={style}>
+		<div class="column has-background-212121 m-5 p-3 has-border-dark rounded" style={{ minHeight: "75vh"}}>
 			<div class="mb-4">
 				<h1 class="title is-7 mb-2 has-text-grey" style={{ display: "inline"}}>{title}</h1>
-				<h1 class="subtitle is-7 mb-2 has-text-grey ml-2 pl-1 pr-1" style={{ display: "inline", borderStyle: "solid", borderWidth: "1px", borderRadius: "1000px"}}>1</h1>
+				<h1 class="subtitle is-7 mb-2 has-text-grey ml-2 pl-1 pr-1 inline has-border-grey circular">1</h1>
 			</div>
 			{children}
 		</div>
 	)
-}
-
-const style = {
-	borderStyle: "solid",
-	borderColor: "#292929",
-	minHeight: "77vh",
-	backgroundColor: "#212121",
-	height: "100%",
-	borderRadius: "8px"
 }
--- react-app/src/components/TicketThumbnail.tsx
+++ react-app/src/components/TicketThumbnail.tsx
@@ -6,7 +6,7 @@ interface TicketThumbnailProps {
 
 export default function TicketThumbnail({ summary, identifier }) {
 	return (
-		<div class="box mt-2 mb-0 p-2 py-3 is-clickable" style={{ backgroundColor: "#454853", borderRadius: "5px", borderStyle: "solid", borderWidth: "1px", borderColor: "#292929"}}>
+		<div class="box mt-2 mb-0 p-2 py-3 is-clickable has-background-474A54 rounded has-border-dark">
 			<h1 class="title m-0 is-7 has-text-white">#{identifier}</h1>
 			<p class="content is-5 has-text-white">{summary}</p>
 		</div>
